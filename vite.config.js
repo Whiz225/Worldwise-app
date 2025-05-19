849adf6b97533159ed7fd4c5df3d0409bc3e0de0
Whiz225 <nwosuemmanuel1994@gmail.com>
2025-05-19 19:50:41 +0100

    Fix-3: Sidebar file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Worldwise-app/",
+  base: "/",
+  // base: "/Worldwise-app/",
   plugins: [react(), eslint],
 });

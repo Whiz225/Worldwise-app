556379ed4be86e48fddd560b2f49468058c8651a
Whiz225 <nwosuemmanuel1994@gmail.com>
2025-05-19 19:18:08 +0100

    Fix: Sidebar file

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,4 +1,4 @@
-import Sidebar from "../components/sidebar";
+import Sidebar from "../components/Sidebar";
 import Map from "../components/Map";
 import styles from "./AppLayout.module.css";
 import User from "../components/User";
